8e8b89f45ac92e9ba5a0503c5e2b2c2ed1d1bfb9
Sohail Chamadia <sohailyc@gmail.com>
2017-04-01 23:22:11 +0530

    Update map.js

--- src/js/map.js
+++ src/js/map.js
@@ -5,18 +5,21 @@ var map;
           center: new google.maps.LatLng(2.8,-187.3),
           mapTypeId: 'terrain'
         });
-      alert("hello1");
-       $.getJSON("http://lidsmysqldb.cloudapp.net/sih2017/lids-api/fetch.php", function(results) {
-      alert(results);
-      alert("hello2");
-      });
+             // Create a <script> tag and set the USGS URL as the source.
+        var script = document.createElement('script');
+        // (In this example we use a locally stored copy instead.)
+        // script.src = 'http://earthquake.usgs.gov/earthquakes/feed/geojsonp/2.5/week';
+        script.src = "http://lidsmysqldb.cloudapp.net/sih2017/lids-api/fetch.php";
+           alert("hello");
+        document.getElementsByTagName('head')[0].appendChild(script);
       }
+     
 
 
       // Loop through the results array and place a marker for each
       // set of coordinates.
       window.eqfeed_callback = function(results) {
-                  alert("hello4");
+        alert(results);
         for (var i = 0; i < results.features.length; i++) {
           var latLng = new google.maps.LatLng(coords[1],coords[0]);
           var marker = new google.maps.Marker({
