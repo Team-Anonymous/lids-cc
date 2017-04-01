d4476da48ab9961c810f01ede53a28e46c42725d
Sohail Chamadia <sohailyc@gmail.com>
2017-04-01 23:23:26 +0530

    Update map.js

--- src/js/map.js
+++ src/js/map.js
@@ -5,12 +5,21 @@ var map;
           center: new google.maps.LatLng(2.8,-187.3),
           mapTypeId: 'terrain'
         });
-      alert("hello1");
+                     // Create a <script> tag and set the USGS URL as the source.
+        var script = document.createElement('script');
+        // (In this example we use a locally stored copy instead.)
+        // script.src = 'http://earthquake.usgs.gov/earthquakes/feed/geojsonp/2.5/week';
+        script.src = "http://lidsmysqldb.cloudapp.net/sih2017/lids-api/fetch.php";
+           alert("hello");
+        document.getElementsByTagName('head')[0].appendChild(script);
+      }
+     
+     /* alert("hello1");
        $.getJSON("http://lidsmysqldb.cloudapp.net/sih2017/lids-api/fetch.php", function(results) {
       alert(results);
       alert("hello2");
       });
-      }
+      }*/
 
 
       // Loop through the results array and place a marker for each
