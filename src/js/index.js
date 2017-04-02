33202bdc82e3476354b0333780b6023d6b23d988
luffykesh <rushikeshpatelc502@gmail.com>
2017-04-02 16:45:28 +0530

    Final Commits
    Final Commits

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   getCFQuotaMetrics();
+  getAlertsAndNotificationsByPriority();
 });
 
 function getCFQuotaMetrics(){
@@ -14,12 +15,12 @@ function getCFQuotaMetrics(){
 
 }
 
-function changeToGreen($rowid){
-  $("rowid").removeClass("first-responder-red").addClass("first-responder-green");
+function changeToGreen(rowid){
+  $("#".rowid).removeClass("first-responder-red").addClass("first-responder-green");
   $("rowid").addClass("first-responder-green");
 }
 
-function getTrafficSignal(){
+/*function getTrafficSignal(){
   $.getJSON("http://lidsmysqldb.cloudapp.net/sih2017/lids-api/fetchTrafficControl.php?signalid=1", function(result){
     var signalStatus = result[0]["SignalStatus"];
     var signalLocation = result[0]["SignalLocation"];
@@ -28,9 +29,55 @@ function getTrafficSignal(){
 
     }
   }
-}
+}*/
 
 function getAlertsAndNotificationsByPriority(){
   // Step 1: Invoke getNOtfications api
   // Step 2: Parse the object 
+  $.getJSON("http://lidsmysqldb.cloudapp.net/sih2017/lids-api/fetchAlerts.php?count=2", function(result){
+	  var timeStamp= result[0]["timestampMs"];
+	  var actiontaken = result[0]["Action"];
+	  var alertPriority = result[0]["AlertPriority"];
+	  var vehicleNumber = result[0]["Number"];
+	  var violationId = result[0]["Violation"];
+	  createAndUpdateTableRow(timeStamp,actiontaken,alertPriority,vehicleNumber,violationId);
+  });
+}
+
+function createAndUpdateTableRow(timeStamp,actiontaken,alertPriority,vehicleNumber,violationId){
+	var alertTable = document.getElementById("alertNotifsTableBody");
+	
+	var alertTR = document.createElement("TR");
+	alertTR.style.backgroundColor="#B3D1FF";
+	
+	var timeStampTD = document.createElement("TD");
+	var newDateTime = new Date();
+	var timeStampText = document.createTextNode(newDateTime);
+	timeStampTD.appendChild(timeStampText);
+	timeStampTD.style.fontSize="10px";
+	alertTR.appendChild(timeStampTD);
+	
+	var ViolationTD = document.createElement("TD");
+	var violationText = document.createTextNode(violationId);
+	ViolationTD.appendChild(violationText);
+	alertTR.appendChild(ViolationTD);
+	
+	var vehicleNumberTD = document.createElement("TD");
+	var vehicleNumberText = document.createTextNode(vehicleNumber);
+	vehicleNumberTD.appendChild(vehicleNumberText);
+	vehicleNumberTD.style.fontSize="12px";
+	alertTR.appendChild(vehicleNumberTD);
+	
+	var actionTD = document.createElement("TD");
+	var actionText= document.createTextNode("   INFORM PATROL");
+	actionTD.appendChild(actionText);
+	actionTD.style.fontSize="12px";
+	alertTR.appendChild(actionTD);
+	
+	
+	
+	alertTable.appendChild(alertTR);
+	
+	
+	
 }
